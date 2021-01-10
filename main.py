8c4e5a0fd11ef5aa1e40bec7d7f293eea4f5916b
henk50 <abraham.ralf.dieter@gmail.com>
2021-01-10 18:03:04 +0100

    Adding new comment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def main(msg)
-    print (msg)
+def main(msg
+         //adding a different Comment adding the second Comment zu diesem File)
+    print(print a message)
 
 main(begeistert)
 
